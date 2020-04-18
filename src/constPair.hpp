6fc146c278b384a68d5cb33d01f775ebf8677f95
kamkamkil <56363663+kamkamkil@users.noreply.github.com>
2020-04-18 14:29:05 +0200

    dodano cout do pair

--- src/constPair.hpp
+++ src/constPair.hpp
@@ -1,33 +1,39 @@
-#include  <utility>
-
+#include <utility>
+#include <iostream>
 template <typename K, typename V>
 class constPair
 {
-    public:
-        const K key;
-        V value;
-        constPair(K key_)
-        {
-            key = key_;
-        }
-        constPair(K key_,V value_)
-        {
-            key = key_;
-            value = value_;
-        }
-        constPair(const std::pair<K, V> &obj)
-        {
-            key = obj.first;
-            value = obj.secont;
-        }//FIXME
-        void setValue(V value_)
-        {
-            value = value_;
-        }
-    private:
-        bool operator > (constPair const &obj) const   {return key > obj.key;};
-        bool operator < (constPair const &obj) const   {return key < obj.key;};   
-        bool operator <= (constPair const &obj) const {return key <= obj.key;};   
-        bool operator >= (constPair const &obj) const {return key >= obj.key;};   
-        bool operator == (constPair const &obj) const {return key == obj.key;};   
+public:
+    const K key;
+    V value;
+    constPair(K key_)
+    {
+        key = key_;
+    }
+    constPair(K key_, V value_)
+    {
+        key = key_;
+        value = value_;
+    }
+    constPair(const std::pair<K, V> &obj)
+    {
+        key = obj.first;
+        value = obj.secont;
+    } //FIXME
+    void setValue(V value_)
+    {
+        value = value_;
+    }
+
+private:
+    bool operator>(constPair const &obj) const { return key > obj.key; };
+    bool operator<(constPair const &obj) const { return key < obj.key; };
+    bool operator<=(constPair const &obj) const { return key <= obj.key; };
+    bool operator>=(constPair const &obj) const { return key >= obj.key; };
+    bool operator==(constPair const &obj) const { return key == obj.key; };
+    ostream &operator<<(ostream &os, const constPair<K,V> &dt)
+    {
+        os << "[ " << K << ", " << V  << " ]";
+        return os;
+    }
 };
